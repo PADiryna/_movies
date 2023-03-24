255540c4ceecdab36c569e100b6d4318a0637d09
Iryna Padafet <irini-84@ukr.net>
2023-03-24 11:23:10 +0100

    Add tests/test_tmdb.py

--- tests/test_tmdb.py
+++ tests/test_tmdb.py
@@ -3,6 +3,8 @@ from main import app
 import pytest
 import requests
 from unittest.mock import Mock
+from flask import Flask
+
 
 key = 'fab609a99011d2a5301b69f5e86466bc'
 token ='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmYWI2MDlhOTkwMTFkMmE1MzAxYjY5ZjVlODY0NjZiYyIsInN1YiI6IjYzZmJmMDY5ODRmMjQ5MDBhOTQxZjI2NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ed_rAlbtavSLRvYBlFpfIdzrqLCH3SaujU_s_AKazYc'
@@ -32,6 +34,35 @@ def test_get_single_movie_cast():
    single_movie_cast = tmdb_client.get_single_movie_cast(943822)
    assert single_movie_cast is not None
 
+
+def test_get_single_movie(monkeypatch):
+   mock_single_movie = ['Movie']
+   requests_mock = Mock()
+   response = requests_mock.return_value
+   response.json.return_value = mock_single_movie
+   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+   single_movie = tmdb_client.get_single_movie(movie_id="")
+   assert single_movie == mock_single_movie
+
+def test_get_movie_images(monkeypatch):
+   mock_movie_images = ['image1', 'image2']
+   requests_mock = Mock()
+   response = requests_mock.return_value
+   response.json.return_value = mock_movie_images
+   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+   movie_images = tmdb_client.get_movie_images(movie_id="")
+   assert movie_images == mock_movie_images
+
+
+def test_get_single_movie_cast(monkeypatch):
+   mock_single_movie_cast = ['actor1', 'actor2']
+   requests_mock = Mock()
+   response = requests_mock.return_value
+   response.json.return_value = mock_single_movie_cast
+   monkeypatch.setattr("tmdb_client.requests.get", requests_mock)
+   single_movie_cast = tmdb_client.get_single_movie(movie_id="")
+   assert single_movie_cast == mock_single_movie_cast
+
 def call_tmdb_api(endpoint):
    full_url = f"https://api.themoviedb.org/3/{endpoint}"
    headers = {
@@ -44,3 +75,12 @@ def call_tmdb_api(endpoint):
 def get_single_movie(movie_id):
    return call_tmdb_api(f"movie/{movie_id}")
 
+
+def test_homepage(monkeypatch):
+   api_mock = Mock(return_value={'results': []})
+   monkeypatch.setattr("tmdb_client.call_tmdb_api", api_mock)
+
+   with app.test_client() as client:
+       response = client.get('/')
+       assert response.status_code == 200
+       api_mock.assert_called_once_with('movie/popular')
